28406ef96324e54f2a00778d4040050721d57e51
Jeanne Allen <tebazele@gmail.com>
2023-02-24 15:52:40 -0700

    added styling

---
+++ main.js
@@ -0,0 +1,21 @@
+const hero = {
+    name: "Pikachu",
+    damage: 5,
+    health: 100,
+    gold: 0,
+    image: "images/pikachu.png"
+}
+
+const boss = {
+    name: 'Big Boss',
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1,
+    image: "images/"
+}
+
+function drawHero() {
+
+}
+
